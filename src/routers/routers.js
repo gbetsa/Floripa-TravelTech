5ba6b10420813e046e78f08a556b5c799c860f8b
Guilherme Betsa <ybtyza@gmail.com>
2026-03-05 05:13:43 -0300

    feat: 🎸 implementação de documentação Swagger API

--- src/routers/routers.js
+++ src/routers/routers.js
@@ -3,6 +3,11 @@ const router = express.Router();
 
 const startupRoutes = require('./startupRoutes');
 
+const swaggerUi = require('swagger-ui-express');
+const swaggerSpec = require('../docs/swagger');
+
 router.use('/api/startup', startupRoutes);
+
+router.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 module.exports = router;
